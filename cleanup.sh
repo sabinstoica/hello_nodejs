165e748f4318edf37853b6a977aa9ea978683673
sabinstoica <sabin_stoica@yahoo.com>
2022-03-22 14:10:00 +0200

    cleanup.sh added

---
+++ cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+container=nodejs_container
+for rcontainers in $(docker ps | grep $container | awk '{print $1}'); do docker stop $rcontainers; done
+for containers in $(docker ps -a | grep $container | awk '{print $1}'); do docker rm $containers; done
+for images in $(docker images | grep $image | awk '{print $3}'); do docker rmi $images; done
